22e2070c763846a9be22428bb280a11aa9b4b704
mileniagoncalves <mileniagoncalves62@gmail.com>
2025-03-02 22:20:43 +0700

    Add files via upload

---
+++ DP session 2 Task 1.py
@@ -0,0 +1,8 @@
+a=10
+b=7
+c=3
+
+if (a>b or b>c) and (a%2==0 and c%2!=0) and (b!=c):
+    print('conditions met')
+else:
+    print('conditions not met')
---
+++ DP session 2 Task 2.py
@@ -0,0 +1,9 @@
+x="Library Nusa Putra"
+y=2025
+z=200.25
+y=int(y)
+z=float(z)
+if isinstance(x,str) and isinstance(y,int) and isinstance(z,float):
+    print ("valid input types")
+else:
+    print ("invalid input types")
